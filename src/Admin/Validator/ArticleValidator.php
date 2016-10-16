e6d02c6d6e403a67a87223814dc8ac0f78a948c7
djavolak <djavolak666@gmail.com>
2016-10-16 20:09:17 +0200

    article validator with validator interface proposal

---
+++ src/Admin/Validator/ArticleValidator.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types = 1);
+
+namespace Admin\Validator;
+
+use Zend\Validator\Exception;
+
+class ArticleValidator implements \Admin\Validator\ValidatorInterface
+{
+    /**
+     * @var \Zend\Stdlib\ArrayObject
+     */
+    private $messages;
+
+    /**
+     * Perform data validation for article entity.
+     *
+     * @param array $postData request post data
+     *
+     * @throws ValidatorException
+     *
+     * @return void
+     */
+    public function validate($postData) : void
+    {
+        $textValidator = new \Zend\Validator\StringLength();
+        $textValidator->setMin(4);
+
+        $this->messages = new \Zend\Stdlib\ArrayObject();
+        if (!$textValidator->isValid($postData['title'])) {
+            $this->messages->offsetSet('title', array_values($textValidator->getMessages()));
+        }
+
+        if (!$textValidator->isValid($postData['lead'])) {
+            $this->messages->offsetSet('lead', array_values($textValidator->getMessages()));
+        }
+
+        //@TODO implement custom validator for article body (html structure) validation
+        if (!$textValidator->isValid($postData['body'])) {
+            $this->messages->offsetSet('body', array_values($textValidator->getMessages()));
+        }
+
+        if ($this->messages->count()) {
+            throw new \Admin\Validator\ValidatorException('Data is not valid');
+        }
+    }
+
+    /**
+     * Messages getter method.
+     *
+     * @return \Zend\Stdlib\ArrayObject
+     */
+    public function getMessages() : \Zend\Stdlib\ArrayObject
+    {
+        return $this->messages;
+    }
+}
