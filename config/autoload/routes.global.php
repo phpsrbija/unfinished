28bf98d67ee53b746f8109696f795e8a2ce9e1eb
djavolak <djavolak666@gmail.com>
2016-10-16 20:09:50 +0200

    configuration for article factories

--- config/autoload/routes.global.php
+++ config/autoload/routes.global.php
@@ -4,15 +4,20 @@ return [
     'dependencies' => [
         'factories' => [
             // Web
-            Web\Action\PingAction::class           => Zend\ServiceManager\Factory\InvokableFactory::class,
-            Web\Action\IndexAction::class          => Web\Factory\Action\IndexFactory::class,
-            Web\Action\AboutAction::class          => Web\Factory\Action\TemplateFactory::class,
-            Web\Action\ContactAction::class        => Web\Factory\Action\TemplateFactory::class,
+            Web\Action\PingAction::class            => Zend\ServiceManager\Factory\InvokableFactory::class,
+            Web\Action\IndexAction::class           => Web\Factory\Action\IndexFactory::class,
+            Web\Action\AboutAction::class           => Web\Factory\Action\TemplateFactory::class,
+            Web\Action\ContactAction::class         => Web\Factory\Action\TemplateFactory::class,
 
             // Admin
-            Admin\Action\IndexAction::class        => Admin\Factory\Action\IndexFactory::class,
-            Admin\Controller\AuthController::class => Admin\Factory\Controller\AuthFactory::class,
+            Admin\Action\IndexAction::class         => Admin\Factory\Action\IndexFactory::class,
+            Admin\Controller\AuthController::class  => Admin\Factory\Controller\AuthFactory::class,
+            Admin\Action\ArticlePageAction::class   => Admin\Factory\Action\ArticlePageFactory::class,
+            Admin\Model\Repository\ArticleRepositoryInterface::class => Admin\Factory\Model\Repository\ArticleRepositoryFactory::class,
+            Admin\Model\Storage\ArticleStorageInterface::class => Admin\Factory\Db\ArticleTableGatewayFactory::class,
 
+            // Db
+            Zend\Db\Adapter\AdapterInterface::class => Zend\Db\Adapter\AdapterServiceFactory::class,
         ],
     ],
 
@@ -54,5 +59,11 @@ return [
             'middleware'      => Admin\Action\IndexAction::class,
             'allowed_methods' => ['GET'],
         ],
+        [
+            'name'            => 'article',
+            'path'            => '/admin/article/:action',
+            'middleware'      => Admin\Action\ArticlePageAction::class,
+            'allowed_methods' => ['GET', 'POST'],
+        ]
     ],
 ];
