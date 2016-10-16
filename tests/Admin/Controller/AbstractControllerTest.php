de26a66410c8793fbee66e61053dd20c47f7aecb
Nemanja Ognjanovic <nemanja@ognjanovic.me>
2016-10-16 19:16:37 +0200

    Admin\Controller\AbstractController tested

--- tests/Admin/Controller/AbstractControllerTest.php
+++ tests/Admin/Controller/AbstractControllerTest.php
@@ -23,9 +23,31 @@ class AbstractControllerTest extends \PHPUnit_Framework_TestCase
         );
         static::assertSame(404, $response->getStatusCode());
     }
+
+    public function testInvokingControllerWithCorrectActionShouldReturnNon404Response()
+    {
+        $request = $this->getMockBuilder('Psr\Http\Message\ServerRequestInterface')
+            ->setMethods(['getAttribute'])
+            ->getMockForAbstractClass();
+        $request->expects(static::at(0))
+            ->method('getAttribute')
+            ->will(static::returnValue('testAction'));
+        $response = $this->getMockBuilder('Psr\Http\Message\ResponseInterface')
+            ->getMockForAbstractClass();
+        $controller = new ControllerStub();
+        $response = $controller(
+            $request,
+            $response,
+            null
+        );
+        static::assertInstanceOf('Zend\Diactoros\Response\EmptyResponse', $response);
+    }
 }
 
 class ControllerStub extends \Admin\Controller\AbstractController
 {
-
+    public function testAction()
+    {
+        return new \Zend\Diactoros\Response\EmptyResponse();
+    }
 }
