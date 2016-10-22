ca27d5503fe37bdc2aad1f9c8e91e0e1f89ececf
Nemanja Ognjanovic <nemanja@ognjanovic.me>
2016-10-22 17:49:48 +0200

    fully tested core error middleware

--- tests/Core/Middleware/ErrorTest.php
+++ tests/Core/Middleware/ErrorTest.php
@@ -21,4 +21,38 @@ class ErrorTest extends \PHPUnit_Framework_TestCase
             $errorMiddleware($exception, $request, $response)
         );
     }
+
+    public function testErrorMiddlewareShouldReturnProperHtmlResponseOnIntStatusCode()
+    {
+        $template = $this->getMockBuilder('Zend\Expressive\Template\TemplateRendererInterface')
+            ->setMethods(['render'])
+            ->getMockForAbstractClass();
+        $template->expects(static::once())
+            ->method('render')
+            ->will(static::returnValue('test'));
+        $errorMiddleware = new \Core\Middleware\Error($template);
+        $request = new \Zend\Diactoros\ServerRequest();
+        $response = new \Zend\Diactoros\Response();
+        static::assertInstanceOf(
+            'Zend\Diactoros\Response\HtmlResponse',
+            $errorMiddleware(506, $request, $response)
+        );
+    }
+
+    public function testErrorMiddlewareShouldReturnProperHtmlResponseOnUnknownTypeOfError()
+    {
+        $template = $this->getMockBuilder('Zend\Expressive\Template\TemplateRendererInterface')
+            ->setMethods(['render'])
+            ->getMockForAbstractClass();
+        $template->expects(static::once())
+            ->method('render')
+            ->will(static::returnValue('test'));
+        $errorMiddleware = new \Core\Middleware\Error($template);
+        $request = new \Zend\Diactoros\ServerRequest();
+        $response = new \Zend\Diactoros\Response();
+        static::assertInstanceOf(
+            'Zend\Diactoros\Response\HtmlResponse',
+            $errorMiddleware(new \stdClass(), $request, $response)
+        );
+    }
 }
