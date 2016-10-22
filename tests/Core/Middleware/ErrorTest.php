5895c12b4aa7e9ed83ccd7f7655c6aceac9758c7
Nemanja Ognjanovic <nemanja@ognjanovic.me>
2016-10-22 17:47:08 +0200

    core error middleware tested

---
+++ tests/Core/Middleware/ErrorTest.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+namespace Test\Core\Middleware;
+
+class ErrorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testErrorMiddlewareShouldReturnProperHtmlResponseOnException()
+    {
+        $template = $this->getMockBuilder('Zend\Expressive\Template\TemplateRendererInterface')
+            ->setMethods(['render'])
+            ->getMockForAbstractClass();
+        $template->expects(static::once())
+            ->method('render')
+            ->will(static::returnValue('test'));
+        $errorMiddleware = new \Core\Middleware\Error($template);
+        $exception = new \Exception('error middleware exception', 222);
+        $request = new \Zend\Diactoros\ServerRequest();
+        $response = new \Zend\Diactoros\Response();
+        static::assertInstanceOf(
+            'Zend\Diactoros\Response\HtmlResponse',
+            $errorMiddleware($exception, $request, $response)
+        );
+    }
+}
