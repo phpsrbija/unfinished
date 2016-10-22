9a040b92b0b261b34c48d7d2fde886180c858d4f
Nemanja Ognjanovic <nemanja@ognjanovic.me>
2016-10-22 13:28:32 +0200

    - ext-mbstring was missing from box

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,7 +32,7 @@ echo "
    Install packages
 ────────────────────────────────────────────────────────────────────────────────────────────────────────"
 sudo apt-get install -y nginx >> /dev/null
-sudo apt-get -y install curl php7.0-zip php7.0-fpm php7.0-mcrypt php7.0-curl php7.0-cli php7.0-mysql php7.0-gd php7.0-intl php7.0-xsl php7.0-xdebug
+sudo apt-get -y install curl php7.0-zip php7.0-fpm php7.0-mcrypt php7.0-curl php7.0-cli php7.0-mysql php7.0-gd php7.0-intl php7.0-xsl php7.0-xdebug php7.0-mbstring
 
 echo "
 ────────────────────────────────────────────────────────────────────────────────────────────────────────
