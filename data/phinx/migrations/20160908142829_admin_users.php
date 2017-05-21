69e6480d36256d8a49bae90efc22ea9491734494
Alex <tasmaniski.com>
2017-05-21 21:04:01 +0200

    ..

--- data/phinx/migrations/20160908142829_admin_users.php
+++ data/phinx/migrations/20160908142829_admin_users.php
@@ -24,29 +24,30 @@ class AdminUsers extends AbstractMigration
             ->addIndex(['email'], ['name' => 'email_INDEX'])
             ->create();
 
-        $faker = Faker\Factory::create();
-        $count = rand(100, 150);
-        for($i = 0; $i < $count; $i++) {
-            $id        = $faker->uuid;
-            $mysqluuid = (new Uuid($id))->toFormat(new Binary());
-            $data      = [
-                'admin_user_uuid' => $mysqluuid,
-                'admin_user_id'   => $id,
-                'email'           => $faker->email,
-                'first_name'      => $faker->firstName,
-                'last_name'       => $faker->lastName,
-                'password'        => '$2y$10$jhGH8RXl269ho1CrLaDiregVuW84HegLHmBFUCKTgDQTH2XgPZyBK', //password = testtest
-                'status'          => rand(0, 1),
-                'last_login'      => rand(0, 10) === 7 ? null : $faker->dateTimeBetween('-10 days', 'now')->format('Y-m-d H:i:s'),
-                'created_at'      => $faker->dateTimeBetween('-20 days', '-10 days')->format('Y-m-d H:i:s'),
-            ];
 
-            $this->insert('admin_users', $data);
-        }
-
-        // Insert default user with password testtest
-        $id        = $faker->uuid;
-        $mysqluuid = (new Uuid($id))->toFormat(new Binary());
+        //$faker = Faker\Factory::create();
+        //$count = rand(100, 150);
+        //for($i = 0; $i < $count; $i++) {
+        //    $id        = $faker->uuid;
+        //    $mysqluuid = (new Uuid($id))->toFormat(new Binary());
+        //    $data      = [
+        //        'admin_user_uuid' => $mysqluuid,
+        //        'admin_user_id'   => $id,
+        //        'email'           => $faker->email,
+        //        'first_name'      => $faker->firstName,
+        //        'last_name'       => $faker->lastName,
+        //        'password'        => '$2y$10$jhGH8RXl269ho1CrLaDiregVuW84HegLHmBFUCKTgDQTH2XgPZyBK', //password = testtest
+        //        'status'          => rand(0, 1),
+        //        'last_login'      => rand(0, 10) === 7 ? null : $faker->dateTimeBetween('-10 days', 'now')->format('Y-m-d H:i:s'),
+        //        'created_at'      => $faker->dateTimeBetween('-20 days', '-10 days')->format('Y-m-d H:i:s'),
+        //    ];
+        //
+        //    $this->insert('admin_users', $data);
+        //}
+        //
+        //// Insert default user with password testtest
+        //$id        = $faker->uuid;
+        //$mysqluuid = (new Uuid($id))->toFormat(new Binary());
         $this->execute(
             "insert into admin_users (admin_user_uuid, admin_user_id, first_name,last_name,email,password, status) values " .
             "('$mysqluuid', '$id', 'Unfinished',  'Admin', 'admin@unfinished.com', '$2y$10\$jhGH8RXl269ho1CrLaDiregVuW84HegLHmBFUCKTgDQTH2XgPZyBK', 1)"
