1eaa145abaab5aa7146099013aa5c716efd83032
djavolak <djavolak@mail.ru>
2017-05-22 23:51:08 +0200

    added missing files :S

---
+++ src/Core/Factory/Service/MeetupApiServiceFactory.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Core\Factory\Service;
+
+use Interop\Container\ContainerInterface;
+use \Core\Service\MeetupApiService;
+
+class MeetupApiServiceFactory
+{
+    /**
+     * Executed when factory is invoked.
+     *
+     * @param ContainerInterface $container container
+     * @return MeetupApiService
+     */
+    public function __invoke(ContainerInterface $container): MeetupApiService
+    {
+        $config = $container->get('config')['meetupApi'];
+
+        return new MeetupApiService($config['key']);
+    }
+}
