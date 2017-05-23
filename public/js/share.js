13f57efa124cd9f3adc21af9f392582903af1712
djavolak <djavolak@mail.ru>
2017-05-23 21:28:47 +0200

    added social shares and counts for all article types

---
+++ public/js/share.js
@@ -0,0 +1,87 @@
+//can come from config and be a 'global' var
+var facebookAppId = 221979587895376;
+
+window.fbAsyncInit = function(){
+    FB.init({appId: facebookAppId, status: true, cookie: true, xfbml: true });
+};
+
+(function(d, debug){var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if(d.getElementById(id)) {return;}
+    js = d.createElement('script'); js.id = id;
+    js.async = true;js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
+    ref.parentNode.insertBefore(js, ref);
+}(document, /*debug*/ false));
+
+function postToFbFeed(title, url, image){
+    var obj = {method: 'feed', link: url, picture: image, name: title}; //, description: desc
+    function callback(response){}
+    FB.ui(obj, callback);
+}
+
+function postToTwitter(title, link) {
+    var url = 'http://twitter.com/home?status=' + encodeURIComponent(title + ' => ' + link);
+
+    window.open(url, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400')
+}
+
+function postToLinkedIn(title, link) {
+    // http://www.linkedin.com/shareArticle?mini=true&url=[URL]&title=[TITLE]&source=[SOURCE/DOMAIN]
+    var url = 'http://www.linkedin.com/shareArticle?mini=true&title=' + encodeURIComponent(title) + '&url=' + link
+        + '&source=PHP%20Srbija';
+
+    window.open(url, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=400')
+}
+
+function getFacebookEngagementCount(url) {
+    var count = 0;
+    $.get(url, function(json) {
+        if (typeof json.og_object != "undefined") {
+            count = JSON.stringify(json.og_object.engagement.count);
+        }
+    }, 'JSON');
+
+    return count;
+}
+
+function getLinkedInEngagementCount(url) {
+    var count = 0;
+    $.get(url, function(json) {
+        if (typeof json.count != "undefined") {
+            count = json.count;
+        }
+    }, 'JSON');
+
+    return count;
+}
+
+function setSocialEngagementCount(element) {
+    var count = 0;
+    var lnUrl = 'http://www.linkedin.com/countserv/count/share?url=' + element.prop('href') + '&format=json';
+    var fbUrl = 'http://graph.facebook.com/v2.2/?id=' + element.prop('href') + '&fields=og_object{engagement}';
+    count += getFacebookEngagementCount(fbUrl);
+    // count += getLinkedInEngagementCount(lnUrl);
+
+    $('.total-shares').html(count);
+}
+
+$(document).ready(function() {
+    $('.facebook-share').click(function(e) {
+        e.preventDefault();
+        var elem = $(this).parent();
+        postToFbFeed(elem.data('title'), elem.prop('href'), elem.data('image'));
+    });
+
+    $('.twitter-share').click(function(e) {
+        e.preventDefault();
+        var elem = $(this).parent();
+        postToTwitter(elem.data('title'), elem.prop('href'));
+    });
+
+    $('.linkedin-share').click(function(e) {
+        e.preventDefault();
+        var elem = $(this).parent();
+        postToLinkedIn(elem.data('title'), elem.prop('href'));
+    });
+
+    setSocialEngagementCount($('.social-icons a'));
+});
