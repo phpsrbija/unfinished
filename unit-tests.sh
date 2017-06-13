e6a78e35fe0e8954d14cc1233a59ff26328c1681
djavolak <djavolak@mail.ru>
2017-06-13 22:04:45 +0200

    fixed script

--- unit-tests.sh
+++ unit-tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-packages=( Admin Article Category Core Menu Newsletter Page Web)
-for package in "${packages[@]}"
+#packages=( )
+for package in Admin Article Category Core Menu Newsletter Page Web
     do vendor/bin/phpunit --configuration="packages/$package/tests/phpunit.xml" --bootstrap="packages/$package/tests/bootstrap.php" || exit
 done
