66fe2975a60ea6b83f3251748ebb3c51b7db68cb
Vranac Srdjan <vranac@code4hire.com>
2017-06-30 15:51:11 +0200

    Move code to application.

--- bin/unit-tests-no-coverage.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-packages=(Admin Article Category Meetup Menu Newsletter Page Std Web)
-for package in "${packages[@]}"
-    do vendor/bin/phpunit --configuration="packages/$package/tests/phpunit.xml" --bootstrap="packages/$package/tests/bootstrap.php" || exit
-done
